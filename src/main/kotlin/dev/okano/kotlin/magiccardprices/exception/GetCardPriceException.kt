b412e82d92968788145cee5c78aea7c0b59c5790
William Johnson dos Santos Okano <williamokano@gmail.com>
2020-10-24 04:14:34 +0200

    Added global exception handler

--- src/main/kotlin/dev/okano/kotlin/magiccardprices/exception/GetCardPriceException.kt
+++ src/main/kotlin/dev/okano/kotlin/magiccardprices/exception/GetCardPriceException.kt
@@ -1,4 +1,4 @@
 package dev.okano.kotlin.magiccardprices.exception
 
 class GetCardPriceException(cardName: String, cause: Throwable?) :
-    RuntimeException("Failed to get card $cardName", cause)
+    BusinessException("Failed to get card $cardName", cause)
